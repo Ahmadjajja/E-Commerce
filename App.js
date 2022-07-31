ad4a1bcd2ddb5e9ee9133e818cf7b713e5217462
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-07-31 21:19:20 +0500

    Drawer navigation done.
    Drawer navigation done...
    Feeling Happy after doing this

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
 import { View, Text } from 'react-native'
 import AppNavigator from "./src/navigation/AppNavigation"
-import Login from "./src/screens/authentication/login/Login"
-import SignUp from "./src/screens/authentication/signUp/SignUp"
-import ForgotPassword from "./src/screens/authentication/forgotPassword/ForgotPassword"
-import Home from './src/screens/frontend/home/Home';
+// import Login from "./src/screens/authentication/login/Login"
+// import SignUp from "./src/screens/authentication/signUp/SignUp"
+// import ForgotPassword from "./src/screens/authentication/forgotPassword/ForgotPassword"
+// import Home from './src/screens/frontend/home/Home';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const App = () => {
-  
-const Stack = createNativeStackNavigator();
+
+  // const Stack = createNativeStackNavigator();
   return (
     // <>
     <NavigationContainer>
-      <Stack.Navigator initialRoute="Login">
-        <Stack.Screen name="Login" component={Login}/>
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword}/>
-        <Stack.Screen name="SignUp" component={SignUp}/>
-        <Stack.Screen name="Home" component={Home}/>
+      {/* <Stack.Navigator initialRoute="Login">
+        <Stack.Screen name="Login" component={Login} options={{ title: 'Login Screen' }}/>
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ title: 'Forgot Password Screen' }}/>
+        <Stack.Screen name="SignUp" component={SignUp} options={{ title: 'SignUp Screen' }}/>
+        <Stack.Screen name="Home" component={Home} options={{ title: 'Home Screen' }}/>
 
-      </Stack.Navigator>
-      {/* <AppNavigator /> */}
+      </Stack.Navigator> */}
+      <AppNavigator />
     </NavigationContainer>
     // {/* </> */}
   )
--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 // import { NavigationContainer } from '@react-navigation/native';
+import Login from "../screens/authentication/login/Login"
+import SignUp from "../screens/authentication/signUp/SignUp"
+import ForgotPassword from "../screens/authentication/forgotPassword/ForgotPassword"
+import Home from '../screens/frontend/home/Home';
 
 function HomeScreen() {
   return (
@@ -28,8 +32,10 @@ export default function AppNavigator() {
   return (
     // <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        <Drawer.Screen name="Home" component={Home} options={{ title: 'Home Screen' }} />
+        <Drawer.Screen name="Login" component={Login} options={{ title: 'Login Screen' }} />
+        <Drawer.Screen name="SignUp" component={SignUp} options={{ title: 'SignUp Screen' }}/>
+        <Drawer.Screen name="ForgotPassword" component={ForgotPassword} options={{ title: 'Forgot Password Screen' }}/>
       </Drawer.Navigator>
     // {/* </NavigationContainer> */}
   );
