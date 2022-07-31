6fefe9082d86ff7e00f43a70a12a0fbc51ce36cb
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-07-31 23:05:16 +0500

    contextApi

---
+++ src/context/ContextApi.js
@@ -0,0 +1,22 @@
+import React,{createContext,useState,useContext} from 'react'
+
+const AppContext = createContext();
+
+
+export default function MyContext({children}) {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  return (
+    <AppContext.Provider value={{ authenticated, setAuthenticated}}>    
+        
+        {children}
+    
+    </AppContext.Provider>
+  )
+}
+
+export const useAuthContext = ()=>{
+
+    return useContext(AppContext)
+
+}
